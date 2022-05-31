925b6bcb9630af22df4288904f7bb23a9037635e
Gbel562 <belhadjani@gmail.com>
2022-05-31 15:32:47 +0200

    MVC Project base

---
+++ eLearningAutomotiveWebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eLearningAutomotiveWebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
